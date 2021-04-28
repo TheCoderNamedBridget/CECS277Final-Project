46e2e4be0d74f6c49c6ea3d0fa137b060730aa47
irod97 <53991499+irod97@users.noreply.github.com>
2021-04-28 13:42:03 -0700

    Added new model for Table

--- CECS277FinalProject/src/FilePanel.java
+++ CECS277FinalProject/src/FilePanel.java
@@ -3,15 +3,17 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.table.TableColumn;
+import java.awt.*;
 import java.io.File;
 
 public class FilePanel extends JInternalFrame {
     private static JTable table = new JTable();
     private File[] files;
-    private final FileSystemView fileSystemView = FileSystemView.getFileSystemView();
-    private static DefaultTableModel tableModel;
-    private ListSelectionListener listSelectionListener;
+    private final static FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+    private static FileTableModel fileTableModel;
+    private static ListSelectionListener listSelectionListener;
+    private static boolean cellSizesSet = false;
     private final String[] columns = {
             "Icon",
             "File",
@@ -34,21 +36,46 @@ public class FilePanel extends JInternalFrame {
     }
 
     public static void setTableData(File[] files) {
-        tableModel = new DefaultTableModel();
-
-        tableModel.setColumnIdentifiers(new String[]{"Files Names"});
-
-        Object[] row = new Object[1];
-
-        for (File file : files) {
-
-            row[0] = file.getName();
-
-            tableModel.addRow(row);
-
+        if (fileTableModel == null) {
+            fileTableModel = new FileTableModel();
+            table.setModel(fileTableModel);
         }
-        table.setModel(tableModel);
+        table.getSelectionModel().removeListSelectionListener(listSelectionListener);
+        fileTableModel.setFiles(files);
+        table.getSelectionModel().addListSelectionListener(listSelectionListener);
+        if (!cellSizesSet) {
+            Icon icon = fileSystemView.getSystemIcon(files[0]);
+
+            table.setRowHeight(icon.getIconHeight());
+
+            setColumnWidth(0, -1);
+            setColumnWidth(3, 60);
+            table.getColumnModel().getColumn(3).setMaxWidth(120);
+            setColumnWidth(4, -1);
+            setColumnWidth(5, -1);
+            setColumnWidth(6, -1);
+            setColumnWidth(7, -1);
+            setColumnWidth(8, -1);
+            setColumnWidth(9, -1);
+
+            cellSizesSet = true;
+        }
+        table.setModel(fileTableModel);
+    }
+
+    private static void setColumnWidth(int column, int width) {
+        TableColumn tableColumn = table.getColumnModel().getColumn(column);
+        if (width<0) {
+            JLabel label = new JLabel( (String)tableColumn.getHeaderValue() );
+            Dimension preferred = label.getPreferredSize();
+            width = (int)preferred.getWidth();
+        }
+        tableColumn.setPreferredWidth(width);
+        tableColumn.setMaxWidth(width);
+        tableColumn.setMinWidth(width);
     }
 
 
+
 }
+
