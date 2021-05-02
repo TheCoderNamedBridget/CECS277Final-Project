4c7aaae3e1b948ede3a73a40e320c03b89545725
irod97 <53991499+irod97@users.noreply.github.com>
2021-05-02 02:39:16 -0700

    Added PlaceHolder for Window Title

--- CECS277FinalProject/src/FileFrame.java
+++ CECS277FinalProject/src/FileFrame.java
@@ -1,23 +1,28 @@
-import javax.swing.JInternalFrame;
-import javax.swing.JSplitPane;
+import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 
 public class FileFrame extends JInternalFrame
 {
-	
-	JSplitPane splitPane;
-	public  FileFrame()
-	{
-		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new DirPanel(), new FilePanel());
-		
-		//This line may change based on Windows/macOS/Linux OS's
-        	//this.setTitle(FileSystemView.getFileSystemView().getHomeDirectory().getParentFile().getParentFile().getPath());
-		
-		this.getContentPane().add(splitPane);
-		this.setMaximizable(true);
-		this.setIconifiable(true);
-		this.setClosable(true);
-		this.setSize(200, 200);
-		this.setVisible(true);
-	}
+
+    JSplitPane splitPane;
+    FileSystemView fileSystemView;
+    public  FileFrame()
+    {
+        FilePanel filePanel = new FilePanel();
+        DirPanel dirPanel = new DirPanel();
+        fileSystemView = FileSystemView.getFileSystemView();
+
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, dirPanel , filePanel);
+
+        this.setTitle("C:");
+
+        this.getContentPane().add(splitPane);
+        this.setResizable(true);
+        this.setMaximizable(true);
+        this.setIconifiable(true);
+        this.setClosable(true);
+        this.setSize(750, 500);
+        this.setVisible(true);
+    }
+
 }
