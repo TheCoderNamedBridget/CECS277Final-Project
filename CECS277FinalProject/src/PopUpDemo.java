77aed2c7d32529bd3e4ac21f66980cd7f36d11c8
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-05-05 15:07:03 -0700

    Create PopUpDemo rename, copy, paste ,delete

---
+++ CECS277FinalProject/src/PopUpDemo.java
@@ -0,0 +1,21 @@
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+class PopUpDemo extends JPopupMenu {
+    JMenuItem rename;
+    JMenuItem copy;
+    JMenuItem paste;
+    JMenuItem delete;
+    
+    public PopUpDemo() {
+    	rename = new JMenuItem("Rename");
+    	copy = new JMenuItem("Copy");
+    	paste = new JMenuItem("Paste");
+    	delete = new JMenuItem("Delete");
+        
+        add(rename);
+        add(copy);
+        add(paste);
+        add(delete);
+    }
+}
